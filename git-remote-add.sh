806a12021370f199a7b77cd82fe8855915883891
anonymous anonymous <not4get2read@yandex>
2018-11-11 11:53:13 +0200

    minor shit

--- git-remote-add.sh
+++ git-remote-add.sh
@@ -1,3 +1,11 @@
 git remote add on ../gits/formo.git
 git remote add off ../gits/formo.git
 
+git remote set-url --push off https://github.com/i4get2code/formo.git
+git remote set-url --add --push off ../gits/formo.git
+
+git remote set-url on https://github.com/i4get2code/formo.git
+git remote set-url --push on ../gits/formo.git
+git remote set-url --add --push on https://github.com/i4get2code/formo.git
+
+git remote -v
