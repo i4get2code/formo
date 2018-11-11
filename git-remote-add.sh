939b06f2b5828f4a3b5ea8efac71277e0cbc6829
anonymous anonymous <not4get2read@yandex>
2018-11-11 13:36:00 +0200

    mod

--- git-remote-add.sh
+++ git-remote-add.sh
@@ -1,3 +1,9 @@
+# S means source
+
+# exec your command from whatever place
+S="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+cd $S # necessary
+
 git remote add on ../gits/formo.git
 git remote add off ../gits/formo.git
 
